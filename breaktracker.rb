0a3c7875fcfc6466f1790ad4060b7a9202bd72d9
Hlynur Tryggvason <h@x0r.is>
2015-02-21 21:07:49 +0000

    Adding file writing and comments
    Both new line for day as well as appending to the current day.

--- breaktracker.rb
+++ breaktracker.rb
@@ -1,19 +1,32 @@
 require 'date'
 
-filename = "breakgraph.txt"
+@filename = "breakgraph.txt"
+# regex that matches the line format
 lineregex = /(\d\d\.\d\d\.\d{4}): x+/
 
 def write_new_line
-  puts "writing new line"
+  puts "Today has not been added before, drawing new bar"
+  File.open(@filename, 'a+') do |file|
+    file.puts Date.today.strftime "%d.%m.%Y: x"
+  end
 end
 
 def append_to_last_line
-  puts "appending to last line"
+  puts "Today exists in graph, adding to todays bar"
+  todays_line = Date.today.strftime "%d.%m.%Y: x"
+  File.write(f = @filename, File.read(f).gsub(/#{todays_line}/, Date.today.strftime("%d.%m.%Y: xx")))
 end
 
-if File.exist? filename
-  lastline = IO.readlines(filename)[-1]
+if File.exist? @filename and !File.zero? @filename
+  # get the last line in the file
+  lastline = IO.readlines(@filename)[-1]
+
+  # apply the line regex to it
   regexmatch = lineregex.match lastline
+
+  # capture the date using a regex, see docs here:
+  # http://ruby-doc.org//core-2.2.0/Regexp.html#class-Regexp-label-Capturing
+  # match 0 is the entire line, match 1 is the date
   lastline_date = Date.parse regexmatch[1]
 
   if lastline_date === Date.today
